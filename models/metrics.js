da56ac63b7d81cfa0a0d12f81712c763673de05a
Dave Collier <lardelbow@gmail.com>
2015-04-01 12:33:33 +0100

    Server metrics functionality working - 01042015

--- models/metrics.js
+++ models/metrics.js
@@ -7,20 +7,24 @@ function MetricsDAO(db) {
   // Specify the collection for this model
   var metrics=db.collection("metrics");
   // Model methods
-  // Return a list of servers
+  // Return a distinct list of servers
   this.fetchServerList=function(callback) {
     metrics.distinct("server", function(err,doc) {
       return callback(doc);
     });
   },
-
+  // Return a list of distinct dates for a server
   this.fetchDateList=function(server,callback) {
     metrics.distinct("date",{"server":server}, function(err,doc) {
       return callback(doc);
     });
+  },
+  // Return the server metrics for a server/date(via key) combo
+  this.fetchServerMetrics=function(key,callback) {
+    metrics.findOne({"_id":key}, function(err,doc) {
+      return callback(doc.server_metrics);
+    });
   }
-
-
 }
-
+// Export as a module
 module.exports.MetricsDAO=MetricsDAO;
